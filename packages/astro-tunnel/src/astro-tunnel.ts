e8c5cb63e5bc03e6a72860d6b6ad84be3f5ec8bd
Shinya Fujino <shf0811@gmail.com>
2024-04-25 23:10:41 +0900

    Use defineToolbarApp

--- packages/astro-tunnel/src/astro-tunnel.ts
+++ packages/astro-tunnel/src/astro-tunnel.ts
@@ -1,4 +1,4 @@
-import type { DevToolbarApp } from "astro";
+import { defineToolbarApp } from "astro/toolbar";
 import { closeOnOutsideClick } from "./utils";
 
 const appName = "Astro Tunnel";
@@ -6,10 +6,7 @@ const appDescription = "Expose your local Astro server to the internet";
 const notReadyMessage = "Tunnel not ready";
 const readyMessage = "Tunnel ready at ";
 
-export default {
-	id: "astro-tunnel",
-	name: appName,
-	icon: '<svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%"><stop offset="0%" style="stop-color:#6366F1"/><stop offset="50%" style="stop-color:#8B5CF6"/><stop offset="100%" style="stop-color:#EC4899"/></linearGradient></defs><path d="M0.877075 7.49991C0.877075 3.84222 3.84222 0.877075 7.49991 0.877075C11.1576 0.877075 14.1227 3.84222 14.1227 7.49991C14.1227 11.1576 11.1576 14.1227 7.49991 14.1227C3.84222 14.1227 0.877075 11.1576 0.877075 7.49991ZM7.49991 1.82708C4.36689 1.82708 1.82708 4.36689 1.82708 7.49991C1.82708 10.6329 4.36689 13.1727 7.49991 13.1727C10.6329 13.1727 13.1727 10.6329 13.1727 7.49991C13.1727 4.36689 10.6329 1.82708 7.49991 1.82708Z" fill="url(#gradient)"></path></svg>',
+export default defineToolbarApp({
 	init(canvas, eventTarget) {
 		const windowElement = document.createElement("astro-dev-toolbar-window");
 
@@ -113,4 +110,4 @@ export default {
 
 		closeOnOutsideClick(eventTarget);
 	},
-} satisfies DevToolbarApp;
+});
